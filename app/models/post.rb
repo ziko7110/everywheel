058aeefe278db176708bfa16a1f2d56d381c5f02
ziko7110 <ziko7110@gmail.com>
2020-02-10 15:48:46 +0900

    リンク化

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,25 +1,7 @@
 require 'open-uri'
 
 class Post < ApplicationRecord
-#   # URLにアクセスするためのライブラリの読み込み
-# require 'open-uri'
-# # Nokogiriライブラリの読み込み
-# require 'nokogiri'
 
-# # スクレイピング先のURL
-# url = 'http://www.yahoo.co.jp/'
-
-# charset = nil
-# html = open(url) do |f|
-#   charset = f.charset # 文字種別を取得
-#   f.read # htmlを読み込んで変数htmlに渡す
-# end
-
-# # htmlをパース(解析)してオブジェクトを生成
-# doc = Nokogiri::HTML.parse(html, nil, charset)
-
-# # タイトルを表示
-# @texts=doc.title
   def self.get_title_drop
     url = 'https://clubdrop.jp/schedule/detail/6006'
     charset = nil
@@ -29,7 +11,6 @@ class Post < ApplicationRecord
     end
 
     doc = Nokogiri::HTML.parse(html, nil, charset)
-    # doc.title
     doc.xpath("//span[@class='artist other_artists']").inner_text.split('/')
   end
 
@@ -42,7 +23,6 @@ class Post < ApplicationRecord
     end
 
     doc = Nokogiri::HTML.parse(html, nil, charset)
-    # doc.title
     doc.xpath("//span[@class='artist other_artists']").inner_text.split('/')
   
   end
@@ -56,7 +36,6 @@ class Post < ApplicationRecord
     end
 
     doc = Nokogiri::HTML.parse(html, nil, charset)
-    # doc.title
     doc.xpath("//span[@class='artist other_artists']").inner_text.split('/')
   
   end
