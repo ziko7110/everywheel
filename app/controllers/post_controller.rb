ed26d0f2bdd22371131ec9022b887a98ac116ced
ziko7110 <ziko7110@gmail.com>
2020-02-13 22:55:23 +0900

    イベントタイトル

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,6 +1,7 @@
 class PostController < ApplicationController
   def index
     @title_drop = Post.get_title_drop
+    @event_name = Post.get_event_name_drop
     @title_beyond = Post.get_title_beyond
     @title_varon = Post.get_title_varon
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -37,6 +37,19 @@ class Post < ApplicationRecord
 
     doc = Nokogiri::HTML.parse(html, nil, charset)
     doc.xpath("//span[@class='artist other_artists']").inner_text.split('/')
-  
+  end
+
+  def self.get_event_name_drop
+    url = 'https://clubdrop.jp/schedule/detail/6006'
+    charset = nil
+    html = open(url) do |f|
+      charset = f.charset # 文字種別を取得
+      f.read # htmlを読み込んで変数htmlに渡す
+    end
+
+    doc = Nokogiri::HTML.parse(html, nil, charset)
+    doc.xpath("/html/body/div[1]/div/div[1]/section/div/article/div/h1").inner_text
+    
+    
   end
 end
