282a7acd5b150bc651ed6a5f081776ee495329f8
ziko7110 <ziko7110@gmail.com>
2020-02-14 13:34:36 +0900

    タイトルスクレイピング

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -3,6 +3,8 @@ class PostController < ApplicationController
     @title_drop = Post.get_title_drop
     @event_name = Post.get_event_name_drop
     @title_beyond = Post.get_title_beyond
+    @event_name_beyond = Post.get_event_name_beyond
     @title_varon = Post.get_title_varon
+    @event_name_varon = Post.get_event_name_varon
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,23 @@ require 'open-uri'
 
 class Post < ApplicationRecord
 
+
+
+def self.drop_url
+  @drop_url = 'https://clubdrop.jp/schedule/detail/7079'
+end
+
+def self.beyond_url
+  @beyond_url = 'https://beyond-osaka.jp/schedule/detail/6439'
+end
+
+def self.varon_url
+  @varon_url = 'https://osaka-varon.jp/schedule/detail/5557'
+end
+
+
   def self.get_title_drop
-    url = 'https://clubdrop.jp/schedule/detail/6006'
+    url = drop_url
     charset = nil
     html = open(url) do |f|
       charset = f.charset # 文字種別を取得
@@ -15,7 +30,8 @@ class Post < ApplicationRecord
   end
 
   def self.get_title_beyond
-    url = 'https://beyond-osaka.jp/schedule/detail/6439'
+  
+    url = beyond_url
     charset = nil
     html = open(url) do |f|
       charset = f.charset # 文字種別を取得
@@ -28,7 +44,7 @@ class Post < ApplicationRecord
   end
 
   def self.get_title_varon
-    url = 'https://osaka-varon.jp/schedule/detail/5557'
+    url = varon_url
     charset = nil
     html = open(url) do |f|
       charset = f.charset # 文字種別を取得
@@ -40,7 +56,7 @@ class Post < ApplicationRecord
   end
 
   def self.get_event_name_drop
-    url = 'https://clubdrop.jp/schedule/detail/6006'
+    url = drop_url
     charset = nil
     html = open(url) do |f|
       charset = f.charset # 文字種別を取得
@@ -49,7 +65,29 @@ class Post < ApplicationRecord
 
     doc = Nokogiri::HTML.parse(html, nil, charset)
     doc.xpath("/html/body/div[1]/div/div[1]/section/div/article/div/h1").inner_text
-    
-    
   end
+
+ def self.get_event_name_beyond
+  url = beyond_url
+  charset = nil
+  html = open(url) do |f|
+    charset = f.charset # 文字種別を取得
+    f.read # htmlを読み込んで変数htmlに渡す
+  end
+  doc = Nokogiri::HTML.parse(html, nil, charset)
+  doc.xpath("/html/body/div[1]/div/div[1]/section/div/article/div/h1").inner_text
+ end
+
+ def self.get_event_name_varon
+  url = varon_url
+  charset = nil
+  html = open(url) do |f|
+    charset = f.charset # 文字種別を取得
+    f.read # htmlを読み込んで変数htmlに渡す
+  end
+
+  doc = Nokogiri::HTML.parse(html, nil, charset)
+  doc.xpath("/html/body/div[1]/div/div[1]/section/div/article/div/h1").inner_text.split('/')
+end
+
 end
