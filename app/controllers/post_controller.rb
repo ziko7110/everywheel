0db7f04af0e721e8f582e8c62396299d9bfc1333
ziko7110 <ziko7110@gmail.com>
2020-02-25 15:25:18 +0900

    スクレイピング追加実装

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -6,6 +6,15 @@ class PostController < ApplicationController
     @event_name_beyond = Post.get_event_name_beyond
     @title_varon = Post.get_title_varon
     @event_name_varon = Post.get_event_name_varon
+    @opst_drop = Post.get_title_drop_opst
+    @opst_beyond = Post.get_title_beyond_opst
+    @opst_varon = Post.get_title_varon_opst
+    @price_drop = Post.get_drop_price
+    @price_beyond = Post.get_beyond_price
+    @price_varon = Post.get_varon_price
+    @image_drop = Post.get_drop_image
+    @image_beyond = Post.get_beyond_image
+    @image_varon = Post.get_varon_image
   end
 
   def new
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,15 +5,18 @@ class Post < ApplicationRecord
 
 
 def self.drop_url
-  @drop_url = 'https://clubdrop.jp/schedule/detail/7079'
+  drop_url = Post.last
+  @drop_url = drop_url[:drop_url]
 end
 
 def self.beyond_url
-  @beyond_url = 'https://beyond-osaka.jp/schedule/detail/6439'
+  beyond_url = Post.last
+  @beyond_url = beyond_url[:beyond_url]
 end
 
 def self.varon_url
-  @varon_url = 'https://osaka-varon.jp/schedule/detail/5557'
+  varon_url = Post.last
+  @varon_url = varon_url[:varon_url]
 end
 
 
@@ -89,5 +92,117 @@ end
   doc = Nokogiri::HTML.parse(html, nil, charset)
   doc.xpath("/html/body/div[1]/div/div[1]/section/div/article/div/h1").inner_text.split('/')
 end
+
+def self.get_title_drop_opst
+  url = drop_url
+  charset = nil
+  html = open(url) do |f|
+    charset = f.charset # 文字種別を取得
+    f.read # htmlを読み込んで変数htmlに渡す
+  end
+
+  doc = Nokogiri::HTML.parse(html, nil, charset)
+  doc.xpath("/html/body/div[1]/div/div[1]/section/div/article/div/dl[2]/dd").inner_text
+end
+
+def self.get_title_beyond_opst
+  url = beyond_url
+  charset = nil
+  html = open(url) do |f|
+    charset = f.charset # 文字種別を取得
+    f.read # htmlを読み込んで変数htmlに渡す
+  end
+  
+  doc = Nokogiri::HTML.parse(html, nil, charset)
+  doc.xpath("/html/body/div[1]/div/div[1]/section/div/article/div/dl[2]/dd").inner_text
+end
+
+def self.get_title_varon_opst
+  url = varon_url
+  charset = nil
+  html = open(url) do |f|
+    charset = f.charset # 文字種別を取得
+    f.read # htmlを読み込んで変数htmlに渡す
+  end
+  
+  doc = Nokogiri::HTML.parse(html, nil, charset)
+  doc.xpath("/html/body/div[1]/div/div[1]/section/div/article/div/dl[2]/dd").inner_text
+end
+
+def self.get_drop_price
+  url = drop_url
+  charset = nil
+  html = open(url) do |f|
+    charset = f.charset # 文字種別を取得
+    f.read # htmlを読み込んで変数htmlに渡す
+  end
+  
+  doc = Nokogiri::HTML.parse(html, nil, charset)
+  doc.xpath("/html/body/div[1]/div/div[1]/section/div/article/div/dl[3]/dd").inner_text
+end
+
+def self.get_beyond_price
+  url = beyond_url
+  charset = nil
+  html = open(url) do |f|
+    charset = f.charset # 文字種別を取得
+    f.read # htmlを読み込んで変数htmlに渡す
+  end
+  
+  doc = Nokogiri::HTML.parse(html, nil, charset)
+  doc.xpath("/html/body/div[1]/div/div[1]/section/div/article/div/dl[3]/dd").inner_text
+end
+
+def self.get_varon_price
+  url = varon_url
+  charset = nil
+  html = open(url) do |f|
+    charset = f.charset # 文字種別を取得
+    f.read # htmlを読み込んで変数htmlに渡す
+  end
+  
+  doc = Nokogiri::HTML.parse(html, nil, charset)
+  doc.xpath("/html/body/div[1]/div/div[1]/section/div/article/div/dl[3]/dd").inner_text
+end
+
+def self.get_drop_image
+  url = drop_url
+  charset = nil
+  html = open(url) do |f|
+    charset = f.charset # 文字種別を取得
+    f.read # htmlを読み込んで変数htmlに渡す
+  end
+  
+  doc = Nokogiri::HTML.parse(html, nil, charset)
+    # image.attribute("src").value
+    doc.xpath("/html/body/div[1]/div/div[1]/section/div/article/div/div/div[1]/a/img").attribute("src").value
+end
+
+def self.get_beyond_image
+  url = beyond_url
+  charset = nil
+  html = open(url) do |f|
+    charset = f.charset # 文字種別を取得
+    f.read # htmlを読み込んで変数htmlに渡す
+  end
+  
+  doc = Nokogiri::HTML.parse(html, nil, charset)
+    # image.attribute("src").value
+    doc.xpath("/html/body/div[1]/div/div[1]/section/div/article/div/div/div[1]/a/img").attribute("src").value
+end
+
+def self.get_varon_image
+  url = varon_url
+  charset = nil
+  html = open(url) do |f|
+    charset = f.charset # 文字種別を取得
+    f.read # htmlを読み込んで変数htmlに渡す
+  end
+  
+  doc = Nokogiri::HTML.parse(html, nil, charset)
+    # image.attribute("src").value
+    doc.xpath("/html/body/div[1]/div/div[1]/section/div/article/div/div/div[1]/a/img").attribute("src").value
+end
+
 
 end
